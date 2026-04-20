3caebdf3be74b2e0d0a2971afc4c8b78e04122e0
Pedro Henrique <79018158+nukhes@users.noreply.github.com>
2026-04-20 20:17:01 -0300

    strict rock_age and color_index requirements for params

--- src/petrology.py
+++ src/petrology.py
@@ -3,6 +3,8 @@ import math
 
 def color_index(m):
     try:
+        if m < 0 or m > 1:
+            raise ValueError("o índice de cor deve estar entre 0 e 1.")
         if m <= 0.1:
             return "hololeucocrática"
         elif m <= 0.3:
@@ -19,6 +21,12 @@ def color_index(m):
 
 
 def rock_age(parent, daughter, hl):
+    for param in [parent, daughter, hl]:
+        if param < 0:
+            raise ValueError("os parâmetros parent, daughter e hl devem ser positivos.")
+    if hl == 0:
+        raise ValueError("o tempo de meia-vida (hl) não pode ser zero.")
+    
     lam = (math.log(2)) / hl
     age = (1 / lam) * math.log(1 + (daughter / parent))
     return age
